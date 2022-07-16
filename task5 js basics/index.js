3f7551c2275b4933c7cee413948ae9526990b71a
Nandhini Rathinam <104826916+nandhinirathinam08@users.noreply.github.com>
2022-07-16 07:56:42 +0530

    Add files via upload

---
+++ task5 js basics/index.js
@@ -0,0 +1,48 @@
+// datatypes
+// var username ="Nandhini";
+// var myNum = 23;
+// var isAvail = true;
+// var initialNumber = null;
+// var initialvar;
+// console.log(username)
+
+
+// operators
+// arithematic operator
+// var val1 = 25;
+// var val3 = 56;
+// var sum = val1+val3;
+// console.log(sum)
+
+// assignment
+// var val3 = 50
+
+// val3 += 30; 
+// val3 -= 30;
+
+// console.log(val3)
+
+//string operator
+// var name1 = "hello";
+// var name2 = "siva"
+
+// var fullname = name1 + name2
+
+// name1 += name2 
+//  console.log(name1)
+
+// array
+// var names = ["nandhu", "shalu", "mano"]
+
+//  console.log(names)
+
+//  variables (let)
+// let name1 = "nandz";
+// name2 = "siva";
+// console.log(name2)
+
+
+//variables (const)
+// logical operator
+var num = 10;
+var myVal= 
